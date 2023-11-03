a77cc05a5b0946859124b9e9fff970a8f4b7d4a5
leonarddei <xinfected27@gmail.com>
2023-11-03 18:27:35 +0100

    Added npm run scripts and fixed Frontend tests

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Test app is running!');
   });
 });
