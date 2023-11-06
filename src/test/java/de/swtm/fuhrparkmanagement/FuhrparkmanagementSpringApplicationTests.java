02be4ace56d9f3ba8d414b1e16313592c43fbec3
leonarddei <xinfected27@gmail.com>
2023-11-06 13:37:53 +0100

    Added tests for the Car repository and controller

--- src/test/java/de/swtm/fuhrparkmanagement/FuhrparkmanagementSpringApplicationTests.java
+++ src/test/java/de/swtm/fuhrparkmanagement/FuhrparkmanagementSpringApplicationTests.java
@@ -1,13 +1,26 @@
 package de.swtm.fuhrparkmanagement;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import de.swtm.fuhrparkmanagement.controller.CarController;
+import de.swtm.fuhrparkmanagement.repository.CarRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FuhrparkmanagementSpringApplicationTests {
 
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private CarController carController;
+
     @Test
     void contextLoads() {
+        assertThat(carRepository).isNotNull();
+        assertThat(carController).isNotNull();
     }
 
 }
