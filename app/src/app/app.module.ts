b3cc910392404ed8ffc2ab0d99b9b96c147fa451
Nick Habermann <nickhabermann@web.de>
2023-11-12 23:00:24 +0100

    Calendar

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,15 +6,16 @@ import { AppComponent } from './app.component';
 import { CalendarComponent } from "./calendar/calendar.component";
 import { MaterialModule } from "./material/material.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarbigComponent } from './calendarbig/calendarbig.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarBigComponent } from './calendar-big/calendar-big.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarBigComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +23,6 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     MaterialModule,
     CalendarComponent,
     BrowserAnimationsModule,
-    CalendarbigComponent,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
