99534dd710a7154849c3a3dbe21674899c7fc7e5
edwinsst <edwin.starz.1@gmail.com>
2023-12-27 23:54:52 +0100

    Added mime attachment with ics file

--- src/main/java/de/swtm/fuhrparkmanagement/service/EmailService.java
+++ src/main/java/de/swtm/fuhrparkmanagement/service/EmailService.java
@@ -1,9 +1,16 @@
 package de.swtm.fuhrparkmanagement.service;
 
+import de.swtm.fuhrparkmanagement.model.RideReservation;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
 @Component
@@ -18,5 +25,31 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+    }
+
+    public void sendMimeMessage(String to, String subject, String text, String icsFileContent)
+            throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        ByteArrayResource resource = new ByteArrayResource(icsFileContent.getBytes(StandardCharsets.UTF_8));
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("test@fuhrparkmanagement.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+        helper.addAttachment("reservierung.ics", resource);
+        emailSender.send(message);
+    }
+
+    public void sendNewReservationEmail(RideReservation rideReservation) throws MessagingException {
+        String str = """
+                Von: %s
+                Bis: %s
+                Purpose: %s
+                """;
+        str = String.format(str, rideReservation.getRide().getStartDate(), rideReservation.getRide().getEndDate(),
+                rideReservation.getRide().getPurpose());
+
+        String icsFileContent = icsService.generateEvent(rideReservation);
+        sendMimeMessage(rideReservation.getUserId() + "@firma.de", "Neue Resevierung", str, icsFileContent);
     }
 }
