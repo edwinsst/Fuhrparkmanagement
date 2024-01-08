27023bec5310a4064e022b46ac413415c9a1d2fe
edwinsst <edwin.starz.1@gmail.com>
2024-01-08 16:11:33 +0100

    Added current month and year information on home view

--- app/src/app/calendar-big/calendar-big.component.ts
+++ app/src/app/calendar-big/calendar-big.component.ts
@@ -116,7 +116,39 @@ export class CalendarBigComponent {
       this.viewDate = subWeeks(this.viewDate, 1);
     }
   }
+  getCurrentMonth(): string {
+    switch (this.viewDate.getMonth()) {
+      case 0:
+        return "Januar";
+      case 1:
+        return "Februar";
+      case 2:
+        return "März";
+      case 3:
+        return "April";
+      case 4:
+        return "Mai";
+      case 5:
+        return "Juni";
+      case 6:
+        return "Juli";
+      case 7:
+        return "August";
+      case 8:
+        return "September";
+      case 9:
+        return "Oktober";
+      case 10:
+        return "November";
+      case 11:
+        return "Dezember";
+    }
+    return ""
+  }
 
+  getCurrentYear(): string {
+    return this.viewDate.toDateString().substring(11,15);
+  }
 }
 
 
